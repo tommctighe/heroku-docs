cae19d671bf0cc495a3b2d74f8cf068ced484049
Tom <mctighe.tom@gmail.com>
2020-05-19 16:22:09 -0700

    testing heroku auto deploy

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -13,7 +13,7 @@
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body (for [kind ["camel" "snake" "kebab"]]
-           (format "change to <a href='/%s?input=%s'> %s</a> case... </a<br />"
+           (format "make it <a href='/%s?input=%s'> %s</a> case... </a<br />"
                    kind sample kind))})
 
 (defroutes app
